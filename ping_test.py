514c2fdc6ef78c0c918daa56436b50ea05f96b0f
FasCo9070 <daguirre1821@gmail.com>
2025-09-05 10:47:16 -0400

    DNS and gateway options added

--- ping_test.py
+++ ping_test.py
@@ -1,9 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import subprocess
 import os
+import time
+
+def user_Input():
+    try:
+        userInput = int(input("> "))
+    except (KeyboardInterrupt):
+        return KeyboardInterrupt
+    except:
+        print("Error! Invalid command!")
+
+    return userInput
+
+def retrieve_Gateway(): #Get default gateway. Assumes rocky linux
+    output = subprocess.run("ip r",capture_output=True).stdout.decode().split()
+    return output[2]
+
 
 def main():
     #Clear terminal
+    #Source: https://stackoverflow.com/questions/2084508/clear-the-terminal-in-python
     os.system('cls' if os.name == 'nt' else 'clear')
     
     while True:
@@ -14,30 +31,25 @@ def main():
 5. Exit/quit the script")
  
         #Input checking
-        try:
-            userInput = int(input("> "))
-        except:
-            print("Error! Invalid command!")
+        userInput = user_Input()
 
         match userInput:
-            case 1:
-                #Display the default gateway
+            case 1: #Display the default gateway
+                print(retrieve_Gateway())
+            case 2: #Test local connectivity
                 print()
-            case 2:
-                #Test local connectivity
+            case 3: #Test remote connectivity
                 print()
-            case 3:
-                #Test remote connectivity
+            case 4: #Test DNS resolution
+                output = subprocess.run(args= "ping www.google.com")
                 print()
-            case 4:
-                #Test DNS resolution
-                print()
-            case 5:
-                #Quit
+            case 5: #Quit
                 print("Quitting...")
                 break
-            case __:
+            case __: #Check for bad command
                 print("Command not recognized!")
+        time.sleep(1)
+        
 
 
 main()
