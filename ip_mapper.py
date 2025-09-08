5af19cde1718217288fdf353dd960528bc4cf897
FasCo9070 <daguirre1821@gmail.com>
2025-09-08 10:38:14 -0400

    Comments and spacing

--- ip_mapper.py
+++ ip_mapper.py
@@ -6,7 +6,7 @@ import time
 LOCK = threading.Lock()
 RESULT = dict()
 
-def ip_sort(a:str,b:str):
+def ip_sort(a:str,b:str): #Disregard
     aInt = a.split(".")[3]
     bInt = b.split(".")[3]
     if aInt > bInt:
@@ -16,14 +16,15 @@ def ip_sort(a:str,b:str):
     else:
         return 0
 
-def ping_ip(ip:str):
+
+def ping_ip(ip:str): #Pings a single ip
     output = subprocess.run("ping "+ip,capture_output=True).returncode
     LOCK.acquire()
     RESULT[ip] = output
     LOCK.release()
 
 
-def start_threads(userInput:list):
+def start_threads(userInput:list): #Iterates over a /24 host
     networkBits = userInput[0]+"."+userInput[1]+"."+userInput[2]+"."
     for host in range(1,256):
         singleHost = networkBits+str(host)
@@ -31,22 +32,22 @@ def start_threads(userInput:list):
         aThread.start()
 
 
-def assemble_result():
+def assemble_result(): 
     output = ""
     # sortedResult = dict(sorted(RESULT.items(), key=ip_sort))
     for key in RESULT.keys():
         if RESULT[key] == 0:
-            ipResult = "There is a host at "+key+"\n"
-            output += ipResult
+            output += "There is a host at "+key+"\n"
     return output
 
 
 def main():
     userInput = input("Enter ip range\n>: ").split(".") #Only works for /24
     start_threads(userInput)
-    while len(RESULT) < 255:
-        time.sleep(1)
+    while len(RESULT) < 255: #Waits until all threads are complete
+        time.sleep(1) #Arbitrary wait time
     # print(RESULT)
     print(assemble_result())
+
 
 main()
