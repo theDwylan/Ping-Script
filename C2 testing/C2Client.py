7455d8fb94624a59c6b850469656f72da4cb00f3
FasCo9070 <daguirre1821@gmail.com>
2025-09-17 13:32:00 -0400

    basic connection

--- C2 testing/C2Client.py
+++ C2 testing/C2Client.py
@@ -1,5 +1,8 @@
 import socket as Socket
 
+PORT = 1234
+HOST = "localhost"
+
 def build_socket(sockType):
     serverSock = Socket.socket(Socket.AF_INET,sockType)
     return serverSock
@@ -10,12 +13,19 @@ def convert_to_bits(toSend:str):
         bitsOut += format(ord(letter),"08b")
     return bitsOut
 
+
 def main():
-    # udpSock = build_socket(Socket.SOCK_DGRAM)
-    # tcpSock = build_socket(Socket.SOCK_STREAM)
+    udpSock = build_socket(Socket.SOCK_DGRAM)
+    tcpSock = build_socket(Socket.SOCK_STREAM)
 
     toSend = input("Message to send: ").strip()
-    print(convert_to_bits(toSend))
+    binaryToSend = convert_to_bits(toSend)
+    for bit in binaryToSend:
+        if bit == 0: #Tcp connect
+            tcpSock.connect((HOST,PORT))
+            tcpSock.close()
+        else: #Udp connect
+            udpSock.sendto(b"",(HOST,PORT))
 
 
 main()
