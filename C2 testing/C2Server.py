67348510e80e7581850404eefd5c359c2e49b9d0
FasCo9070 <daguirre1821@gmail.com>
2025-09-17 10:45:59 -0400

    formatting and comments

--- C2 testing/C2Server.py
+++ C2 testing/C2Server.py
@@ -1,40 +1,40 @@
 import socket as Socket
 import threading
 
-BINARYHOLDER = list()
 EOF = False
 LOCK = threading.Lock()
 
-def handle_UDP(UDPsock):
+def handle_UDP(UDPsock,BinaryHolder):
     global EOF
-    global BINARYHOLDER
     global LOCK
     while True:
         data, addr = UDPsock.recvfrom(1024)
         LOCK.acquire()
         if data != "END" and not EOF:
-            BINARYHOLDER.append(1)
-        else:
+            BinaryHolder.append(1)
+        else: #Message complete. Terminate thread
             EOF = True
             break
         LOCK.release()
 
-def handle_TCP(TCPsock):
+def handle_TCP(TCPsock,BinaryHolder):
     global EOF
-    global BINARYHOLDER
     global LOCK
     while True:
         clientSock, clientAddr = TCPsock.accept()
         LOCK.acquire()
         if clientSock.recv(1024) != b"END" and not EOF:
-            BINARYHOLDER.append(0)
-        else:
+            BinaryHolder.append(0)
+        else: #Message complete. Terminate thread
             EOF = True
             clientSock.close()
             break
         LOCK.release()
         clientSock.close()
 
+def decode(BinaryList): #Convert the collected binary list into a string
+    message = ""
+    return message
 
 
 def main():
@@ -47,9 +47,19 @@ def main():
     serverSocketUDP.setsockopt(Socket.SOL_SOCKET,Socket.SO_REUSEADDR,1)
     serverSocketUDP.bind(("0.0.0.0",1234))
 
-    udp_Thread = threading.Thread(target=handle_UDP,args=(serverSocketUDP,))
-    tcp_Thread = threading.Thread(target=handle_TCP,args=(serverSocketTCP,))
-    udp_Thread.start()
-    tcp_Thread.start()
+    while True:
+        BinaryList = list()
+
+        #This is receiving data
+        udp_Thread = threading.Thread(target=handle_UDP,args=(serverSocketUDP,BinaryList)) #Handles ones
+        tcp_Thread = threading.Thread(target=handle_TCP,args=(serverSocketTCP,BinaryList)) #Handles zeros
+        threads = [udp_Thread,tcp_Thread]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        print(decode(BinaryList)) #Prints what the client sent discretely
+        
+
 
 main()
