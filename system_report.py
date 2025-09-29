93dbd38c750b8be40a6c018ba793463cdb319cf6
FasCo9070 <daguirre1821@gmail.com>
2025-09-29 10:59:21 -0400

    network info

--- system_report.py
+++ system_report.py
@@ -5,9 +5,25 @@ import netifaces
 import socket
 import os
 import subprocess
+import dns.resolver
 
 def get_network_info(networkInfo:list):
-    networkInfo[0] = socket.getfqdn()
+
+    fqdnTokens = socket.getfqdn().split(".") #Gets hostname and domain suffix
+    networkInfo[0] = fqdnTokens[0]
+    for i in range(1,len(fqdnTokens)-1):
+        networkInfo[1] += fqdnTokens[i]+"."
+    networkInfo[1] = networkInfo[1][0:len(networkInfo[1])-1] #Removes extra "."
+
+    iface = netifaces.gateways()['default'][netifaces.AF_INET]
+    networkInfo[3] = iface[0]
+    networkInfo[2] = netifaces.ifaddresses(iface[1])[netifaces.AF_INET][0]['addr']
+    networkInfo[4] = netifaces.ifaddresses(iface[1])[2][0]['netmask']
+
+    DNSservers = dns.resolver.Resolver().nameservers
+    networkInfo[5] = DNSservers[0]
+    networkInfo[6] = DNSservers[1]
+
 
 def get_os_info(OSInfo:list):
     pass
@@ -17,13 +33,15 @@ def get_hardware_info(hardwareInfo:list):
 
 def main():
     #Clear terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
     #Print date
 
-    networkInfo = [] #[Hostname,Domain suffix, ipv4 address, gateway, netmask, Primary dns, secondary dns]
+
+    networkInfo = [""] * 7 #[Hostname ,Domain suffix, ipv4 address, gateway, netmask, Primary dns, secondary dns]
     get_network_info(networkInfo)
-    OSInfo = [] #[OSName, OSVersion, kernalVersion]
+    OSInfo = [""] * 3 #[OSName, OSVersion, kernalVersion]
     get_os_info(OSInfo)
-    hardwareInfo = [] #[diskSize, diskSpace, CPUModel, CPUNumber, CPUCoreCount, totalRAM, availableRAM]
+    hardwareInfo = [""] * 7 #[diskSize, diskSpace, CPUModel, CPUNumber, CPUCoreCount, totalRAM, availableRAM]
     get_hardware_info(hardwareInfo)
 
     #Write to file
